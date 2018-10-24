4b35f8963f87c7e5488cf3b74b8d3ac99423ca63
Arni Magnusson <arnima@hafro.is>
2018-10-24 10:00:34 +0200

    Cosmetic improvements in plots

--- 06_VPA/vpa_script.R
+++ 06_VPA/vpa_script.R
@@ -14,9 +14,9 @@ par(mfrow=c(2,2))
 round(model$N)
 Year <- as.integer(rownames(model$N))
 plot(apply(model$N, 2, median), ylim=c(0,120), yaxs="i", type="l", lty=3,
-     main="Population in 2013 (points) vs.\n median population (line)",
+     main="Population in 2013 (bars) vs.\n median population (line)",
      xlab="Age", ylab="Individuals")
-points(c(tail(model$N, 1)))
+points(c(tail(model$N, 1)), type="h", lwd=6)
 
 ## 2 Recruitment
 barplot(model$N[,1], ylab="Individuals at age 1", main="Recruitment")
@@ -25,7 +25,6 @@ barplot(model$N[,1], ylab="Individuals at age 1", main="Recruitment")
 round(model$F, 2)
 plot(colMeans(model$F)/max(colMeans(model$F)), ylim=c(0,1.05), yaxs="i",
      type="l", main="Selectivity", xlab="Age", ylab="Average F at age")
-segments(c(10,14), c(0.3,0.3), c(10,14), c(0.45,0.45), lty=3)
 
 ## 4 Fbar
 plot(Year, model$F[,15], ylim=c(0,0.12), yaxs="i", main="Fbar (10-14)",
