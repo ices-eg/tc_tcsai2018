1894ad27cc415b57974f4f2f05fdeefc857f6fef
Arni Magnusson <arnima@hafro.is>
2018-10-24 09:47:26 +0200

    Add VPA scripts from last year

---
+++ 06_VPA/vpa_basics.R
@@ -0,0 +1,27 @@
+## Read data, prepare matrices
+C <- as.matrix(read.table("bluefin.dat", header=TRUE, check.names=FALSE, row.names=1)) / 1000
+T <- nrow(C)
+A <- ncol(C)
+N <- F <- Z <- matrix(NA_real_, nrow=T, ncol=A, dimnames=dimnames(C))
+
+## Assume F in terminal year=0.1, M=0.14
+F[T,] <- 0.1
+M <- 0.14
+Z <- F + M
+
+## Calculate N in terminal year
+N <- C*Z / (F*(1-exp(-Z)))
+
+## Calculate N and F up to terminal year,
+## assuming F[oldest] = avg(5 preceding ages)
+for(t in (T-1):1)
+{
+  for(a in 1:(A-1))
+  {
+    N[t,a] <- N[t+1,a+1] * exp(M) + C[t,a] * exp(M/2)
+    F[t,a] <- log(N[t,a] / N[t+1,a+1]) - M
+  }
+  F[t,A] <- mean(F[t,A-(1:5)])
+  Z[t,] <- F[t,] + M
+  N[t,A] <- C[t,A]*Z[t,A] / (F[t,A]*(1-exp(-Z[t,A])))
+}
---
+++ 06_VPA/vpa_script.R
@@ -0,0 +1,30 @@
+source("vpa_function.R")
+
+## Read catch at age, change units
+catage <- read.table("bluefin.dat", header=TRUE, check.names=FALSE, row.names=1)
+catage <- catage / 1000
+
+## Run model
+model <- vpa(catage, 0.14, 0.1, 5)
+
+## View results
+par(mfrow=c(2,2))
+
+## 1 Population
+round(model$N)
+Year <- as.integer(rownames(model$N))
+plot(apply(model$N, 2, median), ylim=c(0,120), yaxs="i", type="l", lty=3,
+     main="Population in 2013 (points) vs.\n median population (line)", xlab="Age", ylab="Individuals")
+points(c(tail(model$N, 1)))
+
+## 2 Recruitment
+barplot(model$N[,1], ylab="Individuals at age 1", main="Recruitment")
+
+## 3 Selectivity
+round(model$F, 2)
+plot(colMeans(model$F)/max(colMeans(model$F)), ylim=c(0,1.05), yaxs="i", type="l",
+     main="Selectivity", xlab="Age", ylab="Average F at age")
+segments(c(10,14), c(0.3,0.3), c(10,14), c(0.45,0.45), lty=3)
+
+## 4 Fbar
+plot(Year, model$F[,15], ylim=c(0,0.12), yaxs="i", main="Fbar (10-14)", ylab="Average F at ages 10-14", type="l")
